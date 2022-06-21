8ef2ed9e796d4ac1ec08bd02ec4c3129c7532cfa
kremenakochanova <kremenakochanova@gmail.com>
2022-06-21 22:32:07 +0300

    Metods: printMonth, printMonthTitle, getMonthName, printMonthBody, getStartDay, getTotalNumberOfDays, getNumberOfDaysInMonth, isLeapYear, dayofweek and searchDay

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,164 @@
-//Дати
-//Да се напише програма, която поддържа текстов диалогов режим,
-// позволяващ удобен интерактивен избор на серия от операции с дати:
-//въвеждане на дата от клавиатурата и проверка за коректност
-//извеждане на дата на екрана в избран от потребителя формат
-//допустими формати: dd/MM/yyyy -> 20/04/2021
-//			     MM/dd/yyyy -> 04/20/2021
-//			    dd-MMM-yyyy -> 04-Feb-2021
-//по дата да се определи ден от седмицата
-//по описание с ден от седмицата да се определи датата, която отговаря на него, например:
-//втората събота от месец февруари
-//последният понеделник от месец януари
-//отпечатва календар за даден месец
-//календарът да се извежда прегледно, като си личи коя дата на кой ден от седмицата отговаря
-//При стартиране на програмата потребителя въвежда кой ден от седмицата е 1 януари и спрямо този ден
-// динамично се генерира календара до края на годината.
-// Денят се въвежда като число от 1 до 7, като 1 отговаря на понеделник, а 7 на неделя.
-// Предварително се знае, че годината е високосна.
-//Реализацията на всички функции трябва да се съдържа в дадена програма,
-// не е позволено използването на системна библиотека за работа с дати.
-
 import java.util.Scanner;
 //public class KremiCalendar {
-
     public class Main {
-
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
             System.out.print("Enter full year: ");
-
             int year = scan.nextInt();
             System.out.print("Enter month in number between 1 and 12: ");
-
             int month = scan.nextInt();
             System.out.print("Enter day: ");
-
             int day = scan.nextInt();
-
             if (month < 1 || month > 12 || year < 1980)
                 System.out.println("Wrong input!");
             else
-                System.out.println("year, month, day");
-
+                printMonth(year, month, day);
+        }
+        static void printMonth(int year, int month, int day) {
+            printMonthTitle(year, month, day);
+            printMonthBody(year, month);
+            dayofweek(day, month, year);
+        }
+        static void printMonthTitle(int year, int month, int day) {
+            System.out.println("Izbarniqt den ot sedmicata e: " + dayofweek(day, month, year));
+            System.out.println("         " + getMonthName(month) + " " + year);
+            System.out.println("–––––––––––––––––––––––––––––");
+            System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+            System.out.println(" Mon Tue Wed Thu Fri Sat Sun");
+        }
+        static String getMonthName(int month) {
+            String monthName = null;
+            switch (month) {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+            }
+            return monthName;
+        }
+        static void printMonthBody(int year, int month) {
+            // Get start day of the week for the first date in the month
+            int startDay = getStartDay(year, month);
+            // Get number of days in the month
+            int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
+            // Pad space before the first day of the month
+            int i = 0;
+            for (i = 0; i < startDay; i++)
+                System.out.print("    ");
+            for (i = 1; i <= numberOfDaysInMonth; i++) {
+                if (i < 10)
+                    System.out.print("   " + i);
+                else
+                    System.out.print("  " + i);
+                if ((i + startDay) % 7 == 0)
+                    System.out.println();
+            }
+            System.out.println();
+            //System.out.print("The startDay is:    " + startDay);
+        }
+        static int getStartDay(int year, int month) {
+            int startDay1800 = 2;
+            int totalNumberOfDays = getTotalNumberOfDays(year, month);
+            //Return the start day
+            return (totalNumberOfDays + startDay1800) % 7;
+        }
+        static int getTotalNumberOfDays(int year, int month) {
+            int total = 0;
+            //Get the total days from 1800 to year - 1
+            for (int i = 1800; i < year; i++)
+                if (isLeapYear(i))
+                    total = total + 366;
+                else
+                    total = total + 365;
+            //Add days from January to the month prior to the calendar month
+            for (int i = 1; i < month; i++)
+                total = total + getNumberOfDaysInMonth(year, i);
+            return total;
+        }
+        static int getNumberOfDaysInMonth(int year, int month) {
+            if (month == 1 || month == 3 || month == 5 || month == 7 ||
+                    month == 8 || month == 10 || month == 12)
+                return 31;
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+                return 30;
+            if (month == 2) return isLeapYear(year) ? 29 : 28;
+            return 0; // If month is incorrect
+        }
+        static boolean isLeapYear(int year) {
+            return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+        }
+        //-------------------------------------------------------------------------------
+        static int dayofweek(int day, int month, int year) {
+            int t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+            if (month < 3) {
+                year = year - 1;
+            }
+            int searchDay = (year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7;
+            String dayName;
+            switch (searchDay) {
+                case 1:
+                    dayName = "mon";
+                    break;
+                case 2:
+                    dayName = "tue";
+                    break;
+                case 3:
+                    dayName = "wed";
+                    break;
+                case 4:
+                    dayName = "thu";
+                    break;
+                case 5:
+                    dayName = "fri";
+                    break;
+                case 6:
+                    dayName = "sat";
+                    break;
+                case 0:
+                    dayName = "sun";
+                    break;
+                default:
+                    dayName = "Invalid day";
+                    break;
+            }
+            System.out.print("Izbraniqt den ot sedmicata e: " + dayName);
+            return searchDay;
         }
     }
+
+
+
+
+
 
